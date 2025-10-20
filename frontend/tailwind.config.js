78c7f60443377ad1faed389c54469260cb7716b5
AnvayUparkar <anvaymuparkar.com>
2025-10-20 20:05:41 +0530

    updates 4

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,5 +4,38 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, addUtilities }) {
+      addBase({
+        'button,input:where([type="button"]),input:where([type="reset"]),input:where([type="submit"])': {
+          'appearance': 'button',
+          '-webkit-appearance': 'button',
+          'background-color': 'transparent',
+          'background-image': 'none',
+        },
+        '[type="search"]': {
+          'appearance': 'textfield',
+          '-webkit-appearance': 'textfield',
+          'outline-offset': '-2px',
+        },
+        '::-webkit-search-decoration': {
+          'appearance': 'none',
+          '-webkit-appearance': 'none',
+        },
+        '::-webkit-file-upload-button': {
+          'appearance': 'button',
+          '-webkit-appearance': 'button',
+          'font': 'inherit',
+        },
+        'progress': {
+          // vertical-align: baseline is the browser default and can be removed
+          // when not overridden elsewhere.
+        },
+        'img,svg,video,canvas,audio,iframe,embed,object': {
+          'display': 'block',
+          // vertical-align is ignored on block-level elements, so remove it.
+        },
+      });
+    },
+  ],
 };
