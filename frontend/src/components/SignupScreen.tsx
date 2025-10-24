8b4ea0078c1144016347dd86466190080ecc9454
AnvayUparkar <anvaymuparkar.com>
2025-10-24 16:34:37 +0530

    REMOVE: Eliminate unused OTP request import from SignupScreen component

--- frontend/src/components/SignupScreen.tsx
+++ frontend/src/components/SignupScreen.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../contexts/AuthContext"
-import { requestOTP } from "../api"
 import Loader from "./shared/Loader"
 
 export default function SignupScreen() {
